1546f1003d15fd4cb882a753656eadd5f4bf72b7
mihaiconstantin <c.tinmihai@ymail.com>
2019-03-17 20:49:21 +0100

    Updated `expertise` blade view to use the model

--- resources/views/partials/expertise.blade.php
+++ resources/views/partials/expertise.blade.php
@@ -8,51 +8,24 @@
                 <h2 class="colorlib-heading">Here are some of my expertise</h2>
             </div>
         </div>
+
         <div class="row row-pt-md">
-            <div class="col-md-4 text-center animate-box">
-                <div class="services color-1">
-                    <span class="icon ">
-                        <i class="fas fa-graduation-cap"></i>
-                    </span>
-                    <div class="desc">
-                        <h3>
-                            Academic Research
-                        </h3>
-                        <p> Curently, I'm appointed as a PhD candidate at the department of Health Promotion at Maastricht
-                            University.
-                        </p>
+
+            @foreach ($data['expertise'] as $expertise)
+                <div class="col-md-4 text-center animate-box">
+                    <div class="services color-1">
+                        <span class="icon ">
+                            <i class="fas {{ $expertise->icon }}"></i>
+                        </span>
+                        <div class="desc">
+                            <h3>{{ $expertise->title }}</h3>
+                            <p>{{ $expertise->description }}</p>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-md-4 text-center animate-box">
-                <div class="services color-2">
-                    <span class="icon">
-                        <i class="icon-data"></i>
-                    </span>
-                    <div class="desc">
-                        <h3>
-                            Data Science
-                        </h3>
-                        <p> In collaboration with my friend and colleague Mihai Constantin, 
-                            I take freelance Data Science projects. </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 text-center animate-box">
-                <div class="services color-3">
-                    <span class="icon">
-                        <i class="fas fa-users"></i>
-                    </span>
-                    <div class="desc">
-                        <h3>
-                            Data Science + Academic Research = Make Something Useful
-                        </h3>
-                        <p> On my spare time I brainstorm about making an application that can potentially make things better
-                            and easier for people.
-                        </p>
-                    </div>
-                </div>
-            </div>
+                </div>  
+            @endforeach
+            
         </div>
+
     </div>
 </section>
