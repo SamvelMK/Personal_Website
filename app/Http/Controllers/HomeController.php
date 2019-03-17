e357acd3595161fc2436aaace7f1ea45b34a8355
SamvelMk <samuelmxitaryan@gmail.com>
2019-03-17 21:43:23 +0100

    added stat model to index page of home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\About;
 use App\Expertise;
+use App\Stat;
 
 class HomeController extends Controller
 {
     public function index() 
     {
         $about = About::where('display', 1)->first();
-        $expertise = Expertise::all(); 
+        $expertise = Expertise::all();
+        $stats = Stat::all(); 
 
         $data = array(
             'about' => $about,
-            'expertise' => $expertise
+            'expertise' => $expertise,
+            'stats' => $stats,
         );
 
         return view('home.index')->with('data', $data);
