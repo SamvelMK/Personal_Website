69b88b29be8a23e6f4bdbf6a667e7d1caa0f91fc
mihaiconstantin <c.tinmihai@ymail.com>
2019-03-17 20:48:33 +0100

    Updated about partial view to use the `About` model

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,18 +1,15 @@
 <section class="colorlib-about" data-section="about">
-        <div class="colorlib-narrow-content">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                        <div class="col-md-12">
-                            <div class="about-desc">
-                                <p>
-                                    <strong>Hi I'm Samvel Mkhitaryan</strong> I am a social and behavioral scientist with 4+ years of work experience with national and international organizations (e.g., RA Government, UNFPA, UNICEF, WorldVision, WorldBank). 
-                                    I speak four languages (English, Russian, Spanish and Armenian). I am currently focusing on data science specialization and looking for new and exciting opportunities. I am very curious to hear about your organization and projects.
-                                </p>
-                            </div>
+    <div class="colorlib-narrow-content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                    <div class="col-md-12">
+                        <div class="about-desc">
+                           {!! $data['about']->description !!}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
